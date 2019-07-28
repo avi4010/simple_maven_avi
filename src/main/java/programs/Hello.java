e98e43194396189c54822fbfcf97d0234ed1063d
avi4010 <avinash120196@gmail.com>
2019-07-28 04:35:41 +0000

    simple maven project is pushed for the first time

---
+++ src/main/java/programs/Hello.java
@@ -0,0 +1,13 @@
+package programs;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
